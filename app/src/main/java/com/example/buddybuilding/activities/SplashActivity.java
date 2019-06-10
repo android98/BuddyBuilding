d7680031417e0ac2a10f156f4d6169aa85a65010
pooria <pooria.maleki89@yahoo.com>
2019-06-10 16:13:19 +0430

    ok

--- app/src/main/java/com/example/buddybuilding/activities/SplashActivity.java
+++ app/src/main/java/com/example/buddybuilding/activities/SplashActivity.java
@@ -1,15 +1,37 @@
 package com.example.buddybuilding.activities;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.example.buddybuilding.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+
+    private static int SPLASH_SCREEN_TIME_OUT = 2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN
+                , WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN_TIME_OUT);
     }
 }
